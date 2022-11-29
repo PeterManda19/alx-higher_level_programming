44c9e815d796bbe0f2f34dd8d930241094e5865d
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-11-29 21:18:49 +0200

    fixes

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,6 +5,7 @@ def remove_char_at(str, n):
     returns string if number n is less than 0
     returns copy of the string 
     without the character at position n if n > 0"""
+   
     if n < 0:
         return (str)
     return (str[:n] + str[n+1:])
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,6 +4,7 @@ def islower(c):
     """Takes in one alphabetic character.
     Returns boolean True if lower case
     Returns boolean False if upper case"""
+   
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
