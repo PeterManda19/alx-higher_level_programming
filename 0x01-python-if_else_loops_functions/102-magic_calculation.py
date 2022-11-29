1e22481c867496a020c1b539d3e67fdb29e97cb0
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-11-29 21:14:09 +0200

    fix 102

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b, c):
     """Takes in 3 numbers a, b, c 
     and returns c if a is less than b
     returns sum of a and b if c i greater than b
     retuns difference between the result of a to the power of b;
     and c """
+
     if a < b:
         return (c)
     if c > b:
