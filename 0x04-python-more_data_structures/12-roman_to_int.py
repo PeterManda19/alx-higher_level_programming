12391cf8ac4cf63f198a0aaf0ea6233ed5c8a2aa
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-12-07 23:29:26 +0200

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,11 @@
 
 def roman_to_int(roman_string):
     """ converts a Roman numeral to an integer."""
+   
     romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     number = len(roman_string)
     value_int = romans[roman_string[number-1]]
+   
     if (roman_string is None) or (type(roman_string) is not str):
         return 0
 
