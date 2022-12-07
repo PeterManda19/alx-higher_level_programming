cdfdedba29fa9082aa38d22249ef34600ffbdbe3
PeterManda19 <peter.pmanda.manda@gmail.com>
2022-12-07 23:26:03 +0200

    12

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,15 +2,16 @@
 
 
 def weight_average(my_list=[]):
-    """ Takes in a list argument and 
+
+    """Takes in a list argument
     returns the weighted average
     of all integers tuple (<score>, <weight>)"""
     
-    if not my_list:
-        return 0
-
     num = 0
     den = 0
+
+    if not my_list:
+        return 0
 
     for tup in my_list:
         num += tup[0] * tup[1]
