4c51a8af7a24c89e0342ac27fd43e8e07e0b5293
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-01-04 22:40:25 +0200

    0x09-python-everything_is_object

---
+++ 100-magic_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def magic_string(l=[]):
+    '''returns a string “BestSchool” 
+    n times the number of the iteration'''
+    
+    l += ["BestSchool"]
+    return ", ".join(l)
+    
