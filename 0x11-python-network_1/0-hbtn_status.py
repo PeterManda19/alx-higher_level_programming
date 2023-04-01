24312869938105faeb6ab922d60a29f8c93558cc
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-04-01 03:04:43 +0200

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+
+# Import the 'request' module from the 'urllib' package
 from urllib import request
 
+# Check if this script is being run as the main program
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        response = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
+    try:
+        # Use a 'with' statement to open a connection to the URL and assign the response to 'response'
+        with request.urlopen("https://intranet.hbtn.io/status") as response:
+            
+            # Read the response and assign it to 'response'
+            response = response.read()
+            
+            # Print the type of the response to the console with formatting
+            print("Body response:")
+            print("\t- type: {}".format(type(response)))
+            
+            # Print the content of the response to the console with formatting
+            print("\t- content: {}".format(response))
+            
+            # Print the utf-8 encoded content of the response to the console with formatting
+            print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
+    
+    except Exception as e:
+        print(f"Error: {e}")
